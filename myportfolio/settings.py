f9f44daaa3c380fed40fd9a17b021c4e3f861d77
Neha <neharedekar20@gmail.com>
2025-07-20 11:15:36 +0530

    Enable WhiteNoise for static files on Render

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 # 🔹 Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -97,3 +98,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
